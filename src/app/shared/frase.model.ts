a31f160556d05d8dc35dacf22e32f31c33971451
lesimoes <lesimoes0@gmail.com>
2017-12-17 18:34:47 -0200

    koeee

---
+++ src/app/shared/frase.model.ts
@@ -0,0 +1,13 @@
+export class Frase {
+
+  public fraseEng: string;
+  public frasePtBr: string;
+
+  construtor(fraseEng: string, frasePtBr: string){
+    this.fraseEng = fraseEng;
+    this.frasePtBr = frasePtBr;
+  }
+
+  //construtor(public fraseEng: string, public frasePtBr: string) {}
+
+}
