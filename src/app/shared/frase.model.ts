7aad477b6f2cd1973be57bc40829cec9e04dbb77
lesimoes <lesimoes0@gmail.com>
2017-12-18 19:49:28 -0200

    correção do constructor

--- src/app/shared/frase.model.ts
+++ src/app/shared/frase.model.ts
@@ -3,10 +3,10 @@ export class Frase {
   public fraseEng: string;
   public frasePtBr: string;
 
-  construtor(fraseEng: string, frasePtBr: string){
+  constructor(fraseEng: string, frasePtBr: string){
     this.fraseEng = fraseEng;
     this.frasePtBr = frasePtBr;
-  }
+  };
 
   //construtor(public fraseEng: string, public frasePtBr: string) {}
 
